57ed0ef1f981737f422aea31724fc6b411bcc2c0
Jeimmy Garcia <jeimmykthgarcia@gmail.com>
2019-09-19 18:37:24 -0500

    Commit inicial

---
+++ Procedimientos/Conexion.php
@@ -0,0 +1,11 @@
+<?php
+
+function conectar($host,$user,$pass,$base)
+{
+	
+	$con=mysqli_connect($host,$user,$pass) or die("No se Pudo establecer la Conexion");
+
+	mysqli_select_db($con,$base);
+
+	return $con;
+}
